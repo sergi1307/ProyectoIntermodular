0a32c4ddc5f82fd4a1303aaf1ef45b26984c876b
marctb06 <marctodoli06@gmail.com>
2026-01-07 13:28:36 +0100

    Hago el CRUD de usuarios

--- Backend/src/app/Http/Controllers/UserController.php
+++ Backend/src/app/Http/Controllers/UserController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -29,5 +32,52 @@ class UserController extends Controller
             'status' => 'true',
             'message' => 'Usuari creat correctament'
         ], 200);
+    }
+    public function show($id)
+    {
+        $user = User::find($id);
+        
+        if (!$user) {
+            return response()->json(['message' => 'No encontrado'], 404);
+        }
+        
+        return response()->json($user);
+    }
+
+    // Actualizar un usuario por id
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['message' => 'No encontrado'], 404);
+        }
+
+        // Preparamos los datos en el array para actualizarlos
+        $datos = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'registration_date' => $request->registration_date,
+        ];
+        $user->update($datos);
+        return response()->json([
+            'message' => 'Usuario actualizado',
+            'user' => $user
+        ]);
+    }
+
+    // eliminar un usario por id
+    public function destroy($id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['message' => 'No encontrado'], 404);
+        }
+
+        $user->delete();
+
+        return response()->json(['message' => 'Usuario eliminado']);
     }
 }
