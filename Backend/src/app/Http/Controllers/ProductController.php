c79a385db43f4ba95fd5b2f48b63fc9e0e1f8194
sergi130706 <sergi13072006@gmail.com>
2026-01-08 12:56:21 +0100

    Dejamos controlador de producto a medias

--- Backend/src/app/Http/Controllers/ProductController.php
+++ Backend/src/app/Http/Controllers/ProductController.php
@@ -2,9 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    /**
+     * Funció que retorna tots els productes
+     * 
+     * @return json
+     */
+    public function index() {
+        $products = Product::all();
+        return response()->json($products);
+    }
+
+    /**
+     * Funció que retorna un producte
+     * 
+     * @param number $id
+     * @return json
+     */
+    public function show($id) {
+        $product = Product::findOrFail($id);
+        return response()->json($product);
+    }
+
+    /**
+     * Funció per a crear un producte mitjançant:
+     * - id_user
+     * - id_delivery_point
+     * - name
+     * - description
+     * - price
+     * - stock
+     * - image
+     * - type_stock
+     * - state
+     * - publicacion
+     * 
+     * @param Request $request
+     * @return json
+     */
+    public function store(Request $request) {
+        $product = Product::create([
+            
+        ])
+    }
 }
