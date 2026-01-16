320d09c762d6463eb5fd1345a27c43fd5d0cc5f9
Xx4rNaUxX <diablojuega@gmail.com>
2026-01-16 09:01:38 +0100

    mi rama actualizada con el main para conectar mapas con backend

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -5,6 +5,8 @@
     <router-link to="/">Home</router-link>
     <router-link to="/login">Login</router-link>
     <router-link to="/products">Productos</router-link>
+    <router-link to="/mapaGeneral">Mapa General</router-link>
+    <router-link to="/mapaEspecifico">Mapa Especifico</router-link>
   </nav>
   <router-view></router-view>
 </template>
