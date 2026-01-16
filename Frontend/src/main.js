4b2baf384ac0cc7c49623a101af628501b003095
sergi130706 <sergi13072006@gmail.com>
2026-01-16 16:53:08 +0100

    Cambios en el frontend

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './routes/routes.js';
 
+import './assets/css/style.css'; 
+
 const app = createApp(App);
 
 app.use(router);
